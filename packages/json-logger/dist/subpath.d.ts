45cccb0f75a773e1c998268e1429d05e67442193
Ciaran Morinan <c@hash.ai>
2022-07-15 15:34:33 +0100

    fix

--- packages/json-logger/dist/subpath.d.ts
+++
@@ -1,2 +0,0 @@
-declare const logSomething: () => void;
-export { logSomething };
--- packages/json-logger/dist/subpath.js
+++
@@ -1,7 +0,0 @@
-import { doNothing } from "./another-subpath.js";
-const logSomething = () => {
-    doNothing();
-    console.log("Something");
-};
-export { logSomething };
-//# sourceMappingURL=subpath.js.map
